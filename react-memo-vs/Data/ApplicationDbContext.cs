16c848ed08801f390687e558012875b6140e30b1
dahee <daheewa128@gmail.com>
2022-01-18 17:26:29 +0900

    초기 커밋.

---
+++ react-memo-vs/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Duende.IdentityServer.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using react_memo_vs.Models;
+
+namespace react_memo_vs.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions)
+            : base(options, operationalStoreOptions)
+        {
+
+        }
+    }
+}
---
+++ react-memo-vs/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace react_memo_vs.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
